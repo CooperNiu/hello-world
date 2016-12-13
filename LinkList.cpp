fd8f62374026b569d1acdb2cdb369932313c063d
CooperNiu <niuyanno.1@outlook.com>
2016-12-13 21:36:52 +0800

    编辑了链表的操作函数

---
+++ LinkList.cpp
@@ -0,0 +1,15 @@
+/*初始化InitList()
+ * 销毁DestroyList(&L)
+ * 判空ListEmpty(L)
+ * 求长度PriorElem(L, cur_e, &pre_e)
+ * 求后继PriorElem(L, cur_e, &pre_e)
+ * 读数据GetElem(L, i, &e)
+ * LocateElem(L, e, compare())
+ * ListTraverse(ListVisit())
+ *
+ * 加工型
+ * ClearList(&L)
+ * PutElem(L, i, e)
+ * ListInsert(&L, i, e)
+ *
+ * */
